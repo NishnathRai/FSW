33de2e8d7c2fcf8084c435c0c1f1c27c51511889
Davu Nishnath <davunishnath@Davus-MacBook-Air.local>
2026-07-11 14:34:15 +0530

    added project structure

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
-#include <avr/io.h>
-#include <util/delay.h>
 
-#define F_CPU 16000000UL
 
 int main()
 {
-    // PB5 = Arduino Digital Pin 13 (Built-in LED)
-    DDRB |= (1 << PB5);
-
-    while (true)
-    {
-        PORTB |= (1 << PB5);   // LED ON
-        _delay_ms(500);
-
-        PORTB &= ~(1 << PB5);  // LED OFF
-        _delay_ms(500);
-    }
-
     return 0;
 }
